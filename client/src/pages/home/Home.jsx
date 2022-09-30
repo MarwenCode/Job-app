84c35aa1260b925f3e0c73602219eadcff33bfd0
MarwenCode <marwen_jdidi@yahoo.com>
2022-10-01 00:08:20 +0200

    filter search tech dev

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,23 +1,13 @@
 import React, {useContext} from 'react';
 import Devs from '../../components/devs/Devs';
-import Search from '../../components/search/Search';
 import { AppContext } from '../../context/context';
-import Filter from '../../components/filter/Filter';
 import "./home.scss"
 
 const Home = () => {
   const {devs} = useContext(AppContext)
   return (
     <div className='home'>
-<<<<<<< HEAD
-      {/* <Filter />
-      <Search /> */}
-=======
-      {/* <Search /> */}
->>>>>>> 8b7b78b54646736f91069f192f65f10aef675bdb
       <Devs devs={devs}/>
-
-
     </div>
   )
 }
